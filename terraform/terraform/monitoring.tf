fa1155f86d346859499dc2125a3836c241e55906
Bojana Belojevic <bojana.belojevic@prodyna.com>
2020-08-05 12:24:11 +0200

    Added terraform - prometheus, grafana

---
+++ terraform/terraform/monitoring.tf
@@ -0,0 +1,38 @@
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+# helm -n monitoring upgrade --install -f C:\PAC\pac-source\minikube\prometheus\prometheus-config.yaml prometheus stable/prometheus
+# helm -n monitoring upgrade --install -f C:\PAC\pac-source\minikube\grafana\grafana-config.yaml grafana stable/grafana
+
+resource "helm_release" "prometheus" {
+  repository = local.helm_repository_stable
+  chart = "prometheus"
+  namespace = kubernetes_namespace.monitoring.metadata[0].name
+  name = "prometheus"
+
+  depends_on = [
+    helm_release.pac-backend
+  ]
+
+  values = [
+    file("helm/prometheus.yaml")
+  ]
+}
+
+resource "helm_release" "grafana" {
+  repository = local.helm_repository_stable
+  chart = "grafana"
+  namespace = kubernetes_namespace.monitoring.metadata[0].name
+  name = "grafana"
+
+  depends_on = [
+    helm_release.prometheus
+  ]
+
+  values = [
+    file("helm/grafana.yaml")
+  ]
+}
